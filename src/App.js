e255cee420dc864668708d5b52385152e9dd17b6
Ananya Pal <ananya.pal@iemlabs.com>
2024-01-16 17:59:59 +0530

    flexbox done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='con-left'>
+        <div className='upper'>
+          
+          <div className='left'>
+            <div className='up'></div>
+            <div className='low'></div>
+          </div>
+
+          <div className='right'></div>
+        </div>
+        <div className='lower'></div>
+      </div>
+
+      <div className='con-right'></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
